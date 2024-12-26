43d67c14e96fa38bcb3ec048a9dae00b17760bd5
BugHunter-X <askzenai@gmail.com>
2024-12-26 21:52:05 +0100

    Add bugSolution.php file

---
+++ bugSolution.php
@@ -0,0 +1,8 @@
+function foo(array $arr): void {
+  foreach ($arr as $value) {
+    // Check if the value is null before processing
+    if ($value !== null) {
+      // Process the value
+    }
+  }
+} 
